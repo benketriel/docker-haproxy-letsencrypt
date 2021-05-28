ae0ce663cf581dff0e62d8a8e4f533cc15504e73
samuel-blad-sigma <samuel.blad@sigma.se>
2021-05-28 10:48:02 +0200

    haproxy-systemd-wrapper not working, commented out

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,10 @@ if [ "${1#-}" != "$1" ]; then
 	set -- haproxy "$@"
 fi
 
-if [ "$1" = 'haproxy' ]; then
-	# if the user wants "haproxy", let's use "haproxy-systemd-wrapper" instead so we can have proper reloadability implemented by upstream
-	shift # "haproxy"
-	set -- "$(which haproxy-systemd-wrapper)" -p /run/haproxy.pid "$@"
-fi
+# if [ "$1" = 'haproxy' ]; then
+# 	# if the user wants "haproxy", let's use "haproxy-systemd-wrapper" instead so we can have proper reloadability implemented by upstream
+# 	shift # "haproxy"
+# 	set -- "$(which haproxy-systemd-wrapper)" -p /run/haproxy.pid "$@"
+# fi
 
 exec "$@"
